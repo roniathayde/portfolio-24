1bf0c7a75cc1da5595655af5b21ad0b7754b3270
roniathayde <pontes014@gmail.com>
2024-03-19 22:03:25 -0300

    add a form zod interactions erros message

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,34 +1,49 @@
 'use client'
 import Image from 'next/image'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import ImagemHomemSentado from '../../assets/images/imagem-pagina-de-contato.svg'
 import { Loader2, Mail, Phone, User } from 'lucide-react'
-import { Span } from 'next/dist/trace'
 
-type Inputs = {
-  firstName: string
-  lastName: string
-  phone: string
-  email: string
-  message: string
-}
+const fieldsFormSchema = z.object({
+  firstName: z.string().min(1, 'O nome é obrigatório'),
+  lastName: z.string().min(1, 'O último é obrigatório'),
+  phone: z
+    .string()
+    .min(11, 'No mínimo 11 digitos')
+    .max(11, 'No máximo 11 digitos'),
+  email: z
+    .string()
+    .min(1, 'O e-mail é obrigatório')
+    .email('O endereço informado é inválido'),
+  message: z.string().min(1, 'O assunto é obrigatório'),
+}) // tipagem zod
+
+type FieldsFormSchema = z.infer<typeof fieldsFormSchema> // tipagem ts inferenciada com zod
 
 export default function Contato() {
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<Inputs>()
+  } = useForm<FieldsFormSchema>({
+    resolver: zodResolver(fieldsFormSchema),
+  })
 
-  const onSubmit = handleSubmit(
-    ({ firstName, lastName, phone, email, message }) => {
-      console.log(firstName, lastName, phone, email, message)
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(), 1000)
-      })
-    },
-  )
+  function handleSubmitForm({
+    firstName,
+    lastName,
+    email,
+    phone,
+    message,
+  }: FieldsFormSchema) {
+    console.log(firstName, lastName, phone, email, message)
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(true), 1000)
+    })
+  }
 
   console.log('isSubmitting', isSubmitting)
 
@@ -40,47 +55,61 @@ export default function Contato() {
             Entre em contato
           </h1>
           <form
-            onSubmit={onSubmit}
+            onSubmit={handleSubmit(handleSubmitForm)}
             className="flex max-w-[567px] flex-col gap-2"
           >
             <div className="flex flex-col gap-2 lg:flex-row">
-              <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
-                <div className="px-4 py-6">
-                  <User className="size-6 text-slate-500" />
+              <div>
+                <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
+                  <div className="px-4 py-6">
+                    <User className="size-6 text-slate-500" />
+                  </div>
+                  <div className="flex w-full flex-col py-4">
+                    <label
+                      htmlFor="firstName"
+                      className="text-sm font-medium text-slate-500"
+                    >
+                      Primeiro nome
+                    </label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      className="bg-transparent text-base font-semibold text-slate-800 outline-none dark:text-slate-200"
+                      {...register('firstName')}
+                    />
+                  </div>
                 </div>
-                <div className="flex w-full flex-col py-4">
-                  <label
-                    htmlFor="firstName"
-                    className="text-sm font-medium text-slate-500"
-                  >
-                    Primeiro nome
-                  </label>
-                  <input
-                    type="text"
-                    id="firstName"
-                    className="bg-transparent text-base font-semibold text-slate-800 outline-none dark:text-slate-200"
-                    {...register('firstName')}
-                  />
-                </div>
+                {errors.firstName && (
+                  <span className="text-sm text-red-500">
+                    {errors.firstName.message}
+                  </span>
+                )}
               </div>
-              <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
-                <div className="px-4 py-6">
-                  <User className="size-6 text-slate-500" />
+              <div>
+                <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
+                  <div className="px-4 py-6">
+                    <User className="size-6 text-slate-500" />
+                  </div>
+                  <div className="flex w-full flex-col py-4">
+                    <label
+                      htmlFor="lastName"
+                      className="text-sm font-medium text-slate-500"
+                    >
+                      Último nome
+                    </label>
+                    <input
+                      type="text"
+                      id="lastName"
+                      className="bg-transparent text-base font-semibold text-slate-800 outline-none dark:text-slate-200"
+                      {...register('lastName')}
+                    />
+                  </div>
                 </div>
-                <div className="flex w-full flex-col py-4">
-                  <label
-                    htmlFor="lastName"
-                    className="text-sm font-medium text-slate-500"
-                  >
-                    Último nome
-                  </label>
-                  <input
-                    type="text"
-                    id="lastName"
-                    className="bg-transparent text-base font-semibold text-slate-800 outline-none dark:text-slate-200"
-                    {...register('lastName')}
-                  />
-                </div>
+                {errors.lastName && (
+                  <span className="text-sm text-red-500">
+                    {errors.lastName.message}
+                  </span>
+                )}
               </div>
             </div>
             <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
@@ -102,6 +131,11 @@ export default function Contato() {
                 />
               </div>
             </div>
+            {errors.email && (
+              <span className="text-sm text-red-500">
+                {errors.email.message}
+              </span>
+            )}
             <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
               <div className="px-4 py-6">
                 <Phone className="size-6 text-slate-500" />
@@ -114,13 +148,18 @@ export default function Contato() {
                   Celular
                 </label>
                 <input
-                  type="text"
+                  type="string"
                   id="phone"
                   className=" bg-transparent text-base font-semibold text-slate-800 outline-none dark:text-slate-200"
                   {...register('phone')}
                 />
               </div>
             </div>
+            {errors.phone && (
+              <span className="text-sm text-red-500">
+                {errors.phone.message}
+              </span>
+            )}
             <div className="flex rounded border-2 border-slate-500 focus-within:border-slate-600 focus-within:ring-2 focus-within:ring-slate-700 dark:border-slate-300 dark:focus-within:ring-slate-400">
               <div className="flex w-full flex-col p-4">
                 <label
@@ -136,7 +175,11 @@ export default function Contato() {
                 ></textarea>
               </div>
             </div>
-
+            {errors.message && (
+              <span className="text-sm text-red-500">
+                {errors.message.message}
+              </span>
+            )}
             <button
               type="submit"
               className="flex items-center justify-center rounded border-2 border-transparent bg-ocean-500 p-4 text-lg font-bold text-slate-100 outline-none hover:border-ocean-400 focus-visible:border-ocean-400 focus-visible:ring-2 focus-visible:ring-ocean-500"
@@ -146,13 +189,6 @@ export default function Contato() {
                 <Loader2 className="size-6 animate-spin text-slate-50" />
               )}
             </button>
-
-            {errors && (
-              <span className="min-w-full text-sm text-rose-500">
-                Ocorreu um erro tente novamente ou entre em contato com o
-                administrador do site em pontes014@gmail.com
-              </span>
-            )}
           </form>
         </article>
         <article className="flex justify-center">
