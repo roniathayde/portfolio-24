402e674b551f143a8a456e918a8da3b2c10c44c1
roniathayde <pontes014@gmail.com>
2024-03-23 17:32:39 -0300

    add a sitemap with alter other things

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://ronilucas.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
+  changefreq: 'daily',
+  priority: 0.8,
+  autoLastmod: true,
 }
