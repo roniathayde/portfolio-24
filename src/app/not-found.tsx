6ccf0c378abea70ae5dbf97a68b5705da87f398a
roniathayde <pontes014@gmail.com>
2024-03-23 16:47:01 -0300

    add a sitemap and 404 page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-[50vh] items-center justify-center">
+      <h1 className="text-center text-2xl font-semibold text-slate-900 dark:text-slate-200">
+        <span>Acabou ficando perdido...</span> <br /> Página não encontrada.
+      </h1>
+    </div>
+  )
+}
